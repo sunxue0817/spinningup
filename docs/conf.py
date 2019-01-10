570a2492871582e5c792d3cf86d46c7c6ac91d29
caozhen.ai <caozhen.ai@bytedance.com>
2019-01-11 04:05:23 +0800

    add copyright

--- docs/conf.py
+++ docs/conf.py
@@ -79,7 +79,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Spinning Up'
-copyright = '2018, OpenAI —— 曹真翻译'
+copyright = 'OpenAI提供教程，由曹真翻译。'
 author = 'Joshua Achiam'
 
 # The version info for the project you're documenting, acts as replacement for
